c6c985c864229c258c6d19f270c565aad89589ad
cs118 <schung507@gmail.com>
2016-02-16 15:46:33 -0800

    started server skeleton

--- server.c
+++ server.c
@@ -1,7 +1,61 @@
+#include <stdio.h>
+#include <sys/types.h>   // definitions of a number of data types used in socket.h and netinet/in.h
+#include <sys/socket.h>  // definitions of structures needed for sockets, e.g. sockaddr
+#include <netinet/in.h>  // constants and structures needed for internet domain addresses, e.g. sockaddr_in
 #include <stdlib.h>
-#include <stdio.h> 
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/wait.h>	/* for the waitpid() system call */
+#include <signal.h>	/* signal name macros, and the kill() prototype */
+#include <fcntl.h> 
+#include <unistd.h>
 
-int main(int argc, char* argv[]) 
+void error(char *msg)
 {
-    return 0;
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, 
+    	newsockfd, 
+    	portno, pid;
+    	socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    char dgram[512];             // Recv buffer
+
+    if (argc < 2) {
+        fprintf(stderr,"ERROR, no port provided\n");
+        exit(1);
+    }
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);	//create socket
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));	//reset memory
+    //fill in address info
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR on binding");
+     
+    printf("server: waiting for connections...\n");
+     
+    //make sure server is always runner with infinite while loop
+    while(1){
+
+    	//recieve UDP from client
+    	if (recvfrom(sockfd, dgram, sizeof(dgram), 0, (struct sockaddr*) &cli_addr, (socklen_t*) &clilen) < 0)
+            error("ERROR on receiving from client");
+
+
+        //echo input back to client 
+        if (sendto(sockfd, dgram, sizeof(dgram), 0, (struct sockaddr *) &cli_addr, clilen) < 0)
+            error("ERROR on sending");
+        
+    } 
 }
